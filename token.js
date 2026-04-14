3a6c845d7698ec571753f22065188dd8c2992b33
sasukeproject07-boop <sasukeproject07@gmail.com>
2026-04-14 17:15:24 +0700

    Update token.js

--- token.js
+++ token.js
@@ -2,5 +2,6 @@
     "tokens": [
        "8416390720:AAFJ_48J-abUinLbJw-HiwqgWFLFYU2jiGE",
        "8697961615:AAFhCMiY79Y7kvFr4YS-SVU9NvtqCFG-6P0",
+       "8786573739:AAHp-PWg_5DXlIfe-zgf949ZNVGtEihoVWg"
        ]
 }
